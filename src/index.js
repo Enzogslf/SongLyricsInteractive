7874d147d531e7e0a432529f3483baf7b00873dc
enzogslf <cyudef@gmail.com>
2022-11-26 21:20:17 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+function secondsong() {
+    const SecondsongH1 = document.getElementById("songname");
+    SecondsongH1.innerHTML = "Fireside";
+    const SecondsongP = document.getElementById("lyrics");
+    SecondsongP.innerHTML = "I'm not sure if I should" +
+    "<br>"+
+    "Show you what I've found " + 
+    "<br>" + 
+    "Has it gone for good" +
+    "<br>" + 
+    "Or is it comin' back around? " +
+    "<br>" + 
+    "Isn't it hard to make up your mind " +
+    "<br>" + 
+    "When you're losin' " +
+    "<br>" +
+    "And your fuse is fireside?";
+
+}
+
+function firstsong() {
+    const FirstsongH1 = document.getElementById("songname");
+    FirstsongH1.innerHTML = "Hello You";
+    const FirstsongP = document.getElementById("lyrics");
+    FirstsongP.innerHTML = "Hello you" +
+    "<br>" +
+    "Still draggin' out a long goodbye? " +
+    "<br>" +
+    "I ought to apologise" + 
+    "<br>" +
+    "For one of the last times";
+}
+
